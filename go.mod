2ec1702ee002a2e52e524b9fb50517b7b369d81c
Daffa' Alexander <daffaalex22@gmail.com>
2023-12-28 00:13:37 +0700

    Query Success

--- go.mod
+++ go.mod
@@ -3,31 +3,22 @@ module go-spreadsheets
 go 1.17
 
 require (
-	cloud.google.com/go/compute v1.23.3 // indirect
-	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	github.com/felixge/httpsnoop v1.0.4 // indirect
-	github.com/go-logr/logr v1.3.0 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
+	cloud.google.com/go/compute v1.9.0 // indirect
+	github.com/FreeLeh/GoFreeDB v1.0.1 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/s2a-go v0.1.7 // indirect
-	github.com/google/uuid v1.4.0 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
-	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
-	go.opencensus.io v0.24.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.46.1 // indirect
-	go.opentelemetry.io/otel v1.21.0 // indirect
-	go.opentelemetry.io/otel/metric v1.21.0 // indirect
-	go.opentelemetry.io/otel/trace v1.21.0 // indirect
-	golang.org/x/crypto v0.16.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/oauth2 v0.15.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/api v0.154.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.5.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
+	golang.org/x/oauth2 v0.0.0-20220822191816-0ebed06d0094 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.94.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231127180814-3a041ad873d4 // indirect
-	google.golang.org/grpc v1.59.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
-	gopkg.in/Iwark/spreadsheet.v2 v2.0.0-20230915040305-7677e8164883 // indirect
+	google.golang.org/genproto v0.0.0-20220829175752-36a9c930ecbf // indirect
+	google.golang.org/grpc v1.49.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,108 +2,112 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"gopkg.in/Iwark/spreadsheet.v2"
+	freedb "github.com/FreeLeh/GoFreeDB"
+	"github.com/FreeLeh/GoFreeDB/google/auth"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
+type Person struct {
+	Name       string `db:"Name"`
+	Gender     string `db:"Gender"`
+	ClassLevel string `db:"Class Level"`
+	HomeState  string `db:"Home State"`
+	Major      string `db:"Major"`
+	ExCul      string `db:"Extracurricular Activity"`
 }
 
 func main() {
-	data, err := os.ReadFile("client_secret.json")
+	// If using Google OAuth2 Flow.
+	auth, err := auth.NewOAuth2FromFile(
+		"client_secret.json",
+		"token.json",
+		freedb.FreeDBGoogleAuthScopes,
+		auth.OAuth2Config{},
+	)
 	if err != nil {
-		log.Fatalf("Unable to read Client Secret: %v", err)
+		fmt.Printf("Cannot obtain auth from client secret: %v", err)
 	}
 
-	conf, err := google.ConfigFromJSON(data, spreadsheet.Scope)
+	store := freedb.NewGoogleSheetRowStore(
+		auth,
+		"10QcMndAD8j4ms1H2yit-zdgN_a5YBYAt98UH5gIgpWg",
+		"Class Data",
+		freedb.GoogleSheetRowStoreConfig{
+			Columns: []string{
+				"Name",
+				"Gender",
+				"Class Level",
+				"Home State",
+				"Major",
+				"Extracurricular Activity",
+			},
+		},
+	)
+
+	defer store.Close(context.Background())
+
+	// Output variable
+	var output []Person
+
+	// Select all columns for all rows
+	// err = store.
+	// 	Select(&output).
+	// 	Exec(context.Background())
+	// if err != nil {
+	// 	fmt.Printf("Error querying rows: %v", err)
+	// }
+
+	// for _, person := range output {
+	// 	fmt.Println("Person: ", person.Name)
+	// }
+
+	// Filter row
+	// err = store.
+	// 	Select(&output).
+	// 	Where("_rid = ?", 1).
+	// 	Exec(context.Background())
+	// if err != nil {
+	// 	fmt.Printf("Error querying rows: %v", err)
+	// }
+
+	// for _, person := range output {
+	// 	fmt.Println("Person: ", person.Name)
+	// 	fmt.Println("Person: ", person.Gender)
+	// 	fmt.Println("Person: ", person.Major)
+	// 	fmt.Println("Person: ", person.ClassLevel)
+	// 	fmt.Println("Person: ", person.HomeState)
+	// 	fmt.Println("Person: ", person.ExCul)
+	// }
+
+	// Select rows with sorting/order by
+	// ordering := []freedb.ColumnOrderBy{
+	// 	{Column: "Name", OrderBy: freedb.OrderByAsc},
+	// }
+
+	// err = store.
+	// 	Select(&output).
+	// 	OrderBy(ordering).
+	// 	Exec(context.Background())
+	// if err != nil {
+	// 	fmt.Printf("Error querying rows: %v", err)
+	// }
+
+	// for _, person := range output {
+	// 	fmt.Println("Person: ", person.Name)
+	// }
+
+	// Select rows with offset and limit
+	err = store.
+		Select(&output).
+		// Offset(2).
+		Limit(5).
+		Exec(context.Background())
 	if err != nil {
-		log.Fatalf("Unable to create Client Secret: %v", err)
+		fmt.Printf("Error querying rows: %v", err)
 	}
 
-	client := getClient(conf)
-	service := spreadsheet.NewServiceWithClient(client)
-
-	spreadsheet, err := service.FetchSpreadsheet("10QcMndAD8j4ms1H2yit-zdgN_a5YBYAt98UH5gIgpWg")
-	if err != nil {
-		log.Fatalf("Unable to fetch Sheets: %v", err)
-	}
-
-	sheet, err := spreadsheet.SheetByIndex(0)
-	if err != nil {
-		log.Fatalf("Unable to get a sheet: %v", err)
-	}
-
-	for _, row := range sheet.Rows {
-		for _, cell := range row {
-			fmt.Println(cell.Value)
-		}
-	}
-
-	// Update cell content
-	sheet.Update(0, 0, "hogehoge")
-
-	// Make sure call Synchronize to reflect the changes
-	err = sheet.Synchronize()
-	if err != nil {
-		log.Fatalf("Unable to update a cell: %v", err)
+	for _, person := range output {
+		fmt.Println("Person: ", person.Name)
 	}
 }
